e4d49d13482efcd3cd4f320709fd912081c1559d
Jin <jinpark1504@gmail.com>
2018-09-20 23:49:07 -0400

    Added code

--- index.php
+++ index.php
@@ -1,1 +1,77 @@
-Place your code here
+<?php
+$people = array(
+    array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
+    array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
+    array('id'=>3, 'first_name'=>'James', 'last_name'=>'Johnston', 'email'=>'james.johnston@gmail.com'),
+    array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
+    array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
+    );
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Table</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <style>
+        #people {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        #people th, #people td {
+            padding: 12px 10px;
+            border: 1px solid $ddd;
+            padding: ipx;
+        }
+
+        #people th {
+            text-align: left;
+            background-color: #4caf50;
+            color: #fff;
+        }
+
+        #people tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #people tr:hover {
+            background-color: #ddd;
+        }
+    </style>
+</head>
+<body>
+    <table id="people">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>E-mail</th>
+            <th></th>
+        </tr>
+<?php foreach ($people as $person): 
+// // It is available in PHP 7.1
+// foreach ($people as list('id' => $id, 'first_name' => $first, 'last_name' => $last, 'email' => $email)):
+?>
+        <tr>
+            <td name="id"><?=$person['id']?></td>
+            <td name="full_name"><?=$person['first_name'] .' ' .$person['last_name']?></td>
+            <td name="email"><?=$person['email']?></td>
+            <td><button class="btn">Info</button>
+        </tr>
+<?php endforeach; ?>
+    </table>
+
+    <script type="text/javascript">
+        $(function() {
+            $(document).on('click', 'button.btn', function() {
+                let tr = $(this).closest('tr');
+                let name = tr.find('td[name="full_name"]')[0].innerText;
+                let email = tr.find('td[name="email"]')[0].innerText;
+                alert(name + "'s E-mail: " + email);
+            });
+        });
+    </script>
+
+</body>
+</html>
